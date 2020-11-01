3413fcdb71f90e471a1327a9883faa7fb581de17
Eric van Wijk <eric@van-wijk.com>
2020-11-01 10:05:02 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# youtube-playlist-data
+# YouTube Playlist (meta)data
  
+This repo contains a script to fetch YouTube playlist information. It fetches metadata (title, description, etc) and is NOT a video downloader.
+
+## Pre-requisites
+This uses the YouTube Data [API](https://developers.google.com/youtube/v3/docs/), you'll need an API key to access it. This can be obtained [here](https://console.developers.google.com/). You can add the API key to the [settings.json](settings.json.sample) file, so you don't have to pass it to the script:
+```
+{
+    "ApiKey" : "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+}
+```
+
+
+## Running the script
+Get the id of the playlist from the playlist's page:
+![alt text](url.png "Playlist URL")
+
+Run the script by specifying the playlist id e.g.:
+```
+./get_playlist_data.ps1 -PlayListID PLbpi6ZahtOH54xRLxXzDneuhq12k36PFj
+```
---
+++ get_playlist_data.ps1
@@ -0,0 +1,101 @@
+#!/usr/bin/env pwsh
+<# 
+.SYNOPSIS 
+    Creates a list of items in a YouTube playlist
+#> 
+#Requires -Version 7
+
+### Arguments
+param ( 
+    [parameter(Mandatory=$false)][string]$ApiKey=$null,
+    [parameter(Mandatory=$false)][string]$PlaylistID=$null
+) 
+
+function Get-PlaylistName (
+    [parameter(Mandatory=$true)][string]$ApiKey=$null,
+    [parameter(Mandatory=$true)][string]$PlaylistID=$null
+) {
+    $headers = @{
+        'Accept' = 'application/json'
+    }
+    $url = "https://youtube.googleapis.com/youtube/v3/playlists?part=snippet&id=${PlaylistID}&key=${ApiKey}"
+    Write-Verbose "url: $url"
+    $result = (Invoke-RestMethod -Uri $url -Method Get -Headers $headers)
+    
+    return $result.items[0].snippet.title
+}
+function Get-PlaylistData (
+    [parameter(Mandatory=$true)][string]$ApiKey=$null,
+    [parameter(Mandatory=$true)][string]$PlaylistID=$null
+) {
+    $headers = @{
+        'Accept' = 'application/json'
+    }
+    $videos = New-Object -Type System.Collections.ArrayList
+    $batchSize = 50
+    do {
+        $url = "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${PlaylistID}&key=${ApiKey}&maxResults=${batchSize}&pageToken=$($result.nextPageToken)"
+        Write-Verbose "url: $url"
+        $result = (Invoke-RestMethod -Uri $url -Method Get -Headers $headers)
+        foreach ($item in $result.items) {
+            Write-Debug $item
+            Write-Debug $item.snippet
+            $video = $($item.snippet)
+            $videoUrl = "https://www.youtube.com/watch?v=$($item.snippet.resourceId.videoId)"
+            $video = $video | Add-Member -MemberType NoteProperty -Name url -Value $videoUrl -Force
+            Write-Debug "item.snippet: $($item.snippet)"
+            $count = $videos.Add($item.snippet)
+        }
+        if ($count -gt $batchSize) {
+            # Start displaying count after first batch of results
+            Write-Host $count
+        }
+        Write-Verbose "nextPageToken: $($result.nextPageToken)"
+    } while ($result.nextPageToken)
+    
+    return $videos
+}
+
+# Validate input
+Write-Information $MyInvocation.line
+$scriptDirectory = (Split-Path -parent -Path $MyInvocation.MyCommand.Path)
+$configFile = (Join-Path $scriptDirectory "config.json")
+if (Test-Path $configFile) {
+    $config       = (Get-Content $configFile | ConvertFrom-Json -NoEnumerate)
+    if (!$PSBoundParameters.ContainsKey('ApiKey')) {
+        $ApiKey = $config.ApiKey
+    }
+    if (!$PSBoundParameters.ContainsKey('PlaylistID')) {
+        $PlaylistID = $config.PlaylistID
+    }
+} else {
+    Write-Information "$configFile not found"
+}
+if (!($ApiKey -and $PlaylistID)) {
+    Write-Host "Specify ApiKey and PlaylistID via arguments or config.json"
+    Get-Help $MyInvocation.MyCommand.Definition
+    exit
+}
+
+# Get playlist name
+$playListName = Get-PlaylistName -ApiKey $ApiKey -PlaylistID $PlaylistID
+if (!$playListName) {
+    Write-Warning "No playlist found with id $PlaylistID, exiting"
+    exit
+}
+
+# Get playlist data
+Write-Host "Retrieving data for playlist '$playListName'..."
+$videos = Get-PlaylistData -ApiKey $ApiKey -PlaylistID $PlaylistID
+
+# Export to CSV and plain text
+$baseExportName = $(Join-Path (Get-Item (Split-Path -parent -Path $MyInvocation.MyCommand.Path)).FullName "data" "$($playListName -replace " ","-")-$(Get-Date -f "yyyyMMddHHmmss")")  
+$csvFileName = $(Join-Path (Get-Item (Split-Path -parent -Path $MyInvocation.MyCommand.Path)).FullName "data" "export-$(Get-Date -f "yyyy-MM-dd-HHmmss").csv") 
+$csvFileName = "${baseExportName}.csv"
+# Hide multiline description, Excel won't be able to import it :-(
+$videos | Select-Object  -Property position, title, publishedAt, url | Export-Csv -Path "$csvFileName" -NoTypeInformation
+Write-Host "$csvFileName (can be imported into Excel)"
+
+$txtFileName = "${baseExportName}.txt"
+$videos | Format-Table -Property position, title, publishedAt, url, description | Out-File $txtFileName -Width 4096
+Write-Host "$txtFileName"
